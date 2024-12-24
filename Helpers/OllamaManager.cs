a0f79aa37c2155b3a87c2d7146b53d8eb8281727
cw-matiastorres <matiastorres@coowise.com>
2024-12-24 11:33:23 -0300

    Add OllamaManager helper class

---
+++ Helpers/OllamaManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Configuration;
+
+namespace OllamaDataverseEntityChatApp.Helpers
+{
+    public static class OllamaManager
+    {
+        private static string BaseUrl => $"http://{ConfigurationManager.AppSettings["OllamaHost"]}:{ConfigurationManager.AppSettings["OllamaPort"]}";
+        public static Uri GetOllamaUri() => new Uri(BaseUrl);
+
+        public static List<(List<string> text, float[] embedding)> FindMostRelevantChunks(
+            float[] queryEmbedding,
+            Dictionary<int, (List<string> text, float[] embedding)> chunkEmbeddings,
+            int topK)
+        {
+            return chunkEmbeddings
+                .Select(kvp => kvp.Value)
+                .OrderByDescending(chunk => CosineSimilarity(queryEmbedding, chunk.embedding))
+                .Take(topK)
+                .ToList();
+        }
+
+        private static float CosineSimilarity(float[] a, float[] b)
+        {
+            float dotProduct = 0;
+            float normA = 0;
+            float normB = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                dotProduct += a[i] * b[i];
+                normA += a[i] * a[i];
+                normB += b[i] * b[i];
+            }
+            return dotProduct / (float)(Math.Sqrt(normA) * Math.Sqrt(normB));
+        }
+    }
+}
